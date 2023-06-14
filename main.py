1f1bfd903e5602c7c49fb18ccfda44e07940fd21
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2023-06-15 01:22:05 +0700

    Updated Main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
 import uvicorn
-
 from fastapi import FastAPI
 from api.auth import router as auth_router
 from api.profile import router as profile_router
 from api.predict import router as predict_router
-from api.predict2 import router as predict2_router
 from api.snackvidia import router as snackvidia_router
 
 app = FastAPI()
@@ -13,14 +10,11 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(predict_router)
-app.include_router(predict2_router)
 app.include_router(snackvidia_router)
 
 @app.get("/")
 def index():
     return "Hi, welcome to the snacktify API"
 
-# Starting the server
-port = os.environ.get("PORT", 8080)
-print(f"Listening to http://0.0.0.0:{port}")
-uvicorn.run(app, host='0.0.0.0',port=port)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
