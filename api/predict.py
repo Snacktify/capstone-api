3134019f91a58f049e1769853c18f6df3254e758
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2023-06-15 01:18:20 +0700

    Remove Dockerfile, Utils, and Predict

--- api/predict.py
+++ api/predict.py
@@ -1,50 +1,56 @@
-# Using api on api
 import cv2
 import numpy as np
-import requests
-import json
-
 import traceback
 import tensorflow as tf
+from fastapi import FastAPI, Response, UploadFile, APIRouter, Depends, HTTPException, Cookie
+from api.auth import decode_access_token, verify_token
 
-from fastapi import FastAPI, Response, UploadFile, APIRouter
-from .utils import load_image_into_numpy_array
 router = APIRouter()
+
+# Initialize Model
+model = tf.saved_model.load("./api/snackscan/1")
 
 # Predict endpoint
 labels = ['grontol', 'lanting', 'lumpia', 'putu ayu', 'serabi solo', 'wajik']
 
+async def get_current_user(access_token: str = Cookie(None)):
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+    decoded_token = decode_access_token(access_token)
+    return decoded_token
+
 @router.post("/predict_image")
-def predict_image(uploaded_file: UploadFile, response: Response):
+def predict_image(uploaded_file: UploadFile, response: Response, current_user: dict = Depends(get_current_user)):
     try:
         # Checking if it's an image
         if uploaded_file.content_type not in ["image/jpeg", "image/png", "image/jpg"]:
             response.status_code = 400
             return "File is Not an Image"
-        
-        image = load_image_into_numpy_array(uploaded_file.file.read())
+
+        # Read image
+        image = cv2.imdecode(np.frombuffer(uploaded_file.file.read(), np.uint8), cv2.IMREAD_COLOR)
+
+        # Convert BGR to RGB
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         print("Image shape:", image.shape)
-        
+
         # Step 1: Resize image to (150, 150)
-        image = cv2.resize(image, (150, 150))
-        image = image / 255.0
-        
+        image_rgb = cv2.resize(image_rgb, (150, 150))
+        image_rgb = image_rgb / 255.0
+
         # Step 2: Prepare data to model
-        image = np.expand_dims(image, 0)
-        
+        image_rgb = np.expand_dims(image_rgb, 0)
+
         # Step 3: Predict the data
-        json_data = json.dumps({"instances": image.tolist()})
-        endpoint = "http://34.128.89.110:8080/v1/models/snackscan:predict"
-        response = requests.post(endpoint, data=json_data)
-        
-        # Step 4: Change the result your determined API output
-        prediction = tf.argmax(response.json()["predictions"], 1).numpy()
-        result = labels[prediction[0]]
-        
+        output = model.signatures["serving_default"](tf.constant(image_rgb.astype(np.float32)))
+        predictions = output["dense_27"].numpy()
+        prediction = np.argmax(predictions)
+        result = labels[prediction]
+
         return {"prediction": result}
     
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
         return "Internal Server Error"
-
