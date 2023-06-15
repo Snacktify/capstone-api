9a78194b6e7dc4888fc348de59b4450d66c98f13
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2023-06-15 18:29:54 +0700

    Fixed Origin

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://127.0.0.1:5500",
+    "http://34.128.89.110:8080",
+    "https://storage.googleapis.com/static-website-snacktify/Landing.html"
+
     # Tambahkan asal lainnya yang diperlukan
 ]
 
