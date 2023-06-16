6f4935b59f11da2ceb30adfef65d0395dd29c317
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2023-06-16 19:47:48 +0700

    Fixed snackvidia

--- api/snackvidia.py
+++ api/snackvidia.py
@@ -16,6 +16,8 @@ class Snack(BaseModel):
     asal_daerah: Any
     harga: Any
     link_gambar: Any
+    nutrition: Any
+    recipe: Any
 
 def get_db_connection():
     return mysql.connector.connect(**SNACKVIDIA_CONFIG)
@@ -30,23 +32,23 @@ mycursor = mydb.cursor()
 # Endpoint to get all snack data
 @router.get("/snackvidia", response_model=List[Snack])
 def get_snacks(current_user: dict = Depends(get_current_user)):
-    mycursor.execute("SELECT * FROM snackvidia")
+    mycursor.execute("SELECT * FROM datasnackvidia")
     result = mycursor.fetchall()
     snacks = []
     if len(result) == 0:
         return {"message": "No Snack Data"}
     for row in result:
-        objekSnack = Snack(nama_snack=row[0], deskripsi=row[1], rating=row[2], asal_daerah=row[3], harga=row[4], link_gambar=row[5])
+        objekSnack = Snack(nama_snack=row[0], deskripsi=row[1], rating=row[2], asal_daerah=row[3], harga=row[4], link_gambar=row[5], nutrition=row[6], recipe=row[7])
         snacks.append(objekSnack)
     return snacks
 
 # Endpoint to get snack data based on snack name
 @router.get("/snackvidia/{nama_snack}", response_model=Snack)
 def get_snack(nama_snack: str, current_user: dict = Depends(get_current_user)):
-    mycursor.execute("SELECT * FROM snackvidia WHERE nama_snack = %s", (nama_snack,))
+    mycursor.execute("SELECT * FROM datasnackvidia WHERE nama_snack = %s", (nama_snack,))
     result = mycursor.fetchone()
     if result:
-        objekSnack = Snack(nama_snack=result[0], deskripsi=result[1], rating=result[2], asal_daerah=result[3], harga=result[4], link_gambar=result[5])
+        objekSnack = Snack(nama_snack=result[0], deskripsi=result[1], rating=result[2], asal_daerah=result[3], harga=result[4], link_gambar=result[5], nutrition=result[6], recipe=result[7])
         return objekSnack
     else:
         raise HTTPException(status_code=404, detail="Snack not found")
@@ -55,7 +57,7 @@ def get_snack(nama_snack: str, current_user: dict = Depends(get_current_user)):
 @router.post("/snackvidia", response_model=Snack)
 def add_snack(objekSnack: Snack, current_user: dict = Depends(get_current_user)):
     try:
-        query = "INSERT INTO snackvidia (nama_snack, deskripsi, rating, asal_daerah, harga) VALUES (%s, %s, %s, %s, %s)"
+        query = "INSERT INTO datasnackvidia (nama_snack, deskripsi, rating, asal_daerah, harga) VALUES (%s, %s, %s, %s, %s)"
         values = (objekSnack.nama_snack, objekSnack.deskripsi, objekSnack.rating, objekSnack.asal_daerah, objekSnack.harga)
         mycursor.execute(query, values)
         mydb.commit()
@@ -66,7 +68,7 @@ def add_snack(objekSnack: Snack, current_user: dict = Depends(get_current_user))
 # Endpoint to modify snack data
 @router.put("/snackvidia/{nama_snack}", response_model=Snack)
 def update_snack(nama_snack: str, snack_data: Snack, current_user: dict = Depends(get_current_user)):
-    query = "UPDATE snackvidia SET nama_snack = %s, deskripsi = %s, rating = %s, asal_daerah = %s, harga = %s WHERE nama_snack = %s"
+    query = "UPDATE datasnackvidia SET nama_snack = %s, deskripsi = %s, rating = %s, asal_daerah = %s, harga = %s WHERE nama_snack = %s"
     values = (snack_data.nama_snack, snack_data.deskripsi, snack_data.rating, snack_data.asal_daerah, snack_data.harga, nama_snack)
     mycursor.execute(query, values)
     if mycursor.rowcount == 0:
@@ -77,7 +79,7 @@ def update_snack(nama_snack: str, snack_data: Snack, current_user: dict = Depend
 # Endpoint to delete snack data based on snack name
 @router.delete("/snackvidia/{nama_snack}")
 def delete_snack(nama_snack: str, current_user: dict = Depends(get_current_user)):
-    query = "DELETE FROM snackvidia WHERE nama_snack = %s"
+    query = "DELETE FROM datasnackvidia WHERE nama_snack = %s"
     values = (nama_snack,)
     mycursor.execute(query, values)
     if mycursor.rowcount == 0:
